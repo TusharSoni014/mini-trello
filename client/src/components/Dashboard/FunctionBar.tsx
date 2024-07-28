cb156e082e1e829964511e195761c4e64a7c975d
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-07-28 16:46:40 +0530

    created function bar

--- client/src/components/Dashboard/FunctionBar.tsx
+++ client/src/components/Dashboard/FunctionBar.tsx
@@ -1,8 +1,19 @@
+"use client";
+
 import React from "react";
 import { Input } from "../ui/input";
 import { CiSearch } from "react-icons/ci";
+import { Button } from "../ui/button";
+import { MdOutlineDateRange } from "react-icons/md";
+import { BsStars } from "react-icons/bs";
+import { CiFilter } from "react-icons/ci";
+import { IoShareSocialOutline } from "react-icons/io5";
+import { updateCreateTaskDrawerVisibility } from "@/lib/slices/app.slice";
+import { useAppDispatch } from "@/lib/hooks";
+import { IoIosAddCircle } from "react-icons/io";
 
 export default function FunctionBar() {
+  const dispatch = useAppDispatch();
   return (
     <div className="w-full border border-red-500 flex justify-between items-center">
       <div className="__search w-fit bg-white rounded-md flex justify-center items-center overflow-hidden">
@@ -11,8 +22,31 @@ export default function FunctionBar() {
           placeholder="Search"
         />
         <div className="p-2">
-          <CiSearch size={20}/>
+          <CiSearch size={20} />
         </div>
+      </div>
+      <div className="__toolbar flex gap-5 justify-center items-center">
+        <Button size="sm" variant="ghost" className="text-black/40 w-fit p-0">
+          Calendar view <MdOutlineDateRange size={20} />
+        </Button>
+        <Button size="sm" variant="ghost" className="text-black/40 w-fit p-0">
+          Automation <BsStars size={18} />
+        </Button>
+        <Button size="sm" variant="ghost" className="text-black/40 w-fit p-0">
+          Filter <CiFilter size={20} />
+        </Button>
+        <Button size="sm" variant="ghost" className="text-black/40 w-fit p-0">
+          Share <IoShareSocialOutline size={18} />
+        </Button>
+        <Button
+          onClick={() => dispatch(updateCreateTaskDrawerVisibility(true))}
+          className="w-fit gap-1"
+          variant="purple-gradient"
+          size="sm"
+        >
+          Create new
+          <IoIosAddCircle />
+        </Button>
       </div>
     </div>
   );
