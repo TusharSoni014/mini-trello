d108eabbe156e0314530bd1a0c13492f772deb59
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-07-28 16:27:41 +0530

    created header section in dashboard

--- client/src/components/Dashboard/TaskManager.tsx
+++ client/src/components/Dashboard/TaskManager.tsx
@@ -2,6 +2,8 @@ import { IAppSlice } from "@/lib/slices/app.slice";
 import { MdOutlineHelpOutline } from "react-icons/md";
 import React from "react";
 import IntroIcon from "./icons/IntroIcon";
+import ShareIcon from "./icons/ShareIcon";
+import AccessIcon from "./icons/AccessIcon";
 
 export default function TaskManager({
   currentUser,
@@ -20,7 +22,7 @@ export default function TaskManager({
         </div>
       </div>
       <div className="__cards flex gap-3">
-        <div className="__card rounded-lg p-4 bg-white flex gap-3 w-1/3 justify-center items-center">
+        <div className="__card rounded-lg p-4 bg-white flex gap-3 w-full justify-center items-center">
           <div className="shrink-0">
             <IntroIcon />
           </div>
@@ -32,27 +34,29 @@ export default function TaskManager({
             </p>
           </div>
         </div>
-        <div className="__card rounded-lg p-4 bg-white flex gap-3 w-1/3 justify-center items-center">
+        <div className="__card rounded-lg p-4 bg-white flex gap-3 w-full justify-center items-center">
           <div className="shrink-0">
-            <IntroIcon />
+            <ShareIcon />
           </div>
           <div className="flex flex-col gap-1">
-            <h3 className="text-black/70 font-semibold">Introducing tags</h3>
+            <h3 className="text-black/70 font-semibold">
+              Share Notes Instantly
+            </h3>
             <p className="text-black/40 text-xs">
-              Easily categorize and find your notes by adding tags. Keep your
-              workspace clutter-free and efficient.
+              Effortlessly share your notes with others via email or link.
+              Enhance collaboration with quick sharing options.
             </p>
           </div>
         </div>
-        <div className="__card rounded-lg p-4 bg-white flex gap-3 w-1/3 justify-center items-center">
+        <div className="__card rounded-lg p-4 bg-white flex gap-3 w-full justify-center items-center">
           <div className="shrink-0">
-            <IntroIcon />
+            <AccessIcon />
           </div>
           <div className="flex flex-col gap-1">
-            <h3 className="text-black/70 font-semibold">Introducing tags</h3>
+            <h3 className="text-black/70 font-semibold">Access Anywhere</h3>
             <p className="text-black/40 text-xs">
-              Easily categorize and find your notes by adding tags. Keep your
-              workspace clutter-free and efficient.
+              Sync your notes across all devices. Stay productive whether you
+              {"'"}re on your phone, tablet, or computer.
             </p>
           </div>
         </div>
--- client/src/components/Dashboard/icons/IntroIcon.tsx
+++ client/src/components/Dashboard/icons/IntroIcon.tsx
@@ -9,8 +9,8 @@ export default function IntroIcon({
 }) {
   return (
     <svg
-      width={width}
-      height={height}
+      width={`${width}`}
+      height={`${height}`}
       viewBox={`0 0 ${width} ${height}`}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
